1af4acb7ef7a5be2454e8e61fa998fe3bb7fcf5d
kaveesha20 <kaveeshasharadi@gmail.com>
2025-01-01 23:06:15 +0530

    Did changes to supplier page

--- server/models/supplier.js
+++ server/models/supplier.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const supplierSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Supplier name
-  contactPerson: { type: String, required: true }, // Contact person for the supplier
+  company: { type: String, required: true }, // Contact person for the supplier
   contactNumber: { type: String, required: true }, // Contact number
   email: { type: String, required: true, unique: true }, // Supplier email
   address: { type: String, required: true }, // Address of the supplier
-  productsSupplied: { type: [String], required: true }, // List of product names supplied
+  //productsSupplied: { type: [String], required: true }, // List of product names supplied
 });
 
 module.exports = mongoose.model('Supplier', supplierSchema);
