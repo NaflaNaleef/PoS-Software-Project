2fdd5da38217423d50453cd4bc0352e1cf49c562
Kaveesha Nirmani <152583494+Kaveesha20@users.noreply.github.com>
2025-05-05 12:02:45 +0530

    tailwind.css implemntation

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3bb19b', // Your brand color
+        secondary: '#edf5f3' // Light background
+      }
+    },
+  },
+  plugins: [],
+}
