2869d306ac2b830c6ca16f12e7d5c154e12785e4
UmodaKahatagahawatta <piyumiumoda@gmail.com>
2025-05-05 10:43:20 +0530

    swipe card

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const stripePromise = loadStripe("pk_test_51R5U4eED2StRK7aLViqTuosxjsbxJoKo4px42qj00nROwB7Nq7TvzfpU6hOJCXjAJmBR5OEULvgbh9hTglKnXY7u00c7IxsNZQ");
+
+ReactDOM.render(
   <React.StrictMode>
-    
-    <App />
-   
-  </React.StrictMode>
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
 
